aa6fc53d7884220a9915bd811a5ecfb8e7827e2d
_r <122609604+neeeerrd@users.noreply.github.com>
2023-03-05 14:01:26 +0800

    fix cpu usage going above 100%

--- plugins/cpu.sh
+++ plugins/cpu.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sketchybar --set cpu label="$(ps -A -o %cpu | awk '{s+=$1} END {print s "%"}')"
+sketchybar --set cpu label="$(top -l  2 | grep -E "^CPU" | tail -1 | awk '{ print $3 + $5"%" }')"
