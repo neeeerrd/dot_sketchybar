239db72616dcec60631abb39de7c22cb565add27
_r <122609604+neeeerrd@users.noreply.github.com>
2023-03-05 12:00:25 +0800

    cool wifi status indicator

--- plugins/wifi.sh
+++ plugins/wifi.sh
@@ -3,6 +3,11 @@
 # The wifi_change event supplies a $INFO variable in which the current SSID
 # is passed to the script.
 
-WIFI=${INFO:-"Not Connected"}
+WIFI=${INFO:-"Disconnected"}
+if [ $WIFI == "Disconnected" ]; then
+   ICON=􀙈
+else
+   ICON=􀙇
+fi
 
-sketchybar --set $NAME label="${WIFI}"
+sketchybar --set $NAME label="${WIFI}" icon="${ICON}"
